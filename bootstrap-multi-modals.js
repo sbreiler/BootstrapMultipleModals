90cbc9e0db5b3acc2acda73ace047d4842c48882
Steffen Breiler <sbreiler@users.noreply.github.com>
2017-04-04 22:58:58 +0200

    Update bootstrap-multi-modals.js

--- bootstrap-multi-modals.js
+++ bootstrap-multi-modals.js
@@ -1,121 +1,122 @@
 /*!
- * bootstrap-multi-modals v0.1.1 (https://github.com/sbreiler/bootstrap-multi-modals)
+ * bootstrap-multi-modals v0.1.2 (https://github.com/sbreiler/bootstrap-multi-modals)
  *
- * Copyright (c) 2016 Steffen Breiler
+ * Copyright (c) 2017 Steffen Breiler
  * Licensed under MIT (https://github.com/sbreiler/bootstrap-multi-modals/blob/master/LICENSE)
  */
 
 +function ($) {
     'use strict';
 
-    var zindex = 1040;
-	
-	$(document).ready(function() {
-		var modal_list = $('.modal'); // get a list of all existing modals
-		var $body = $('body');
-		var originalBodyPad = parseInt($body.css('padding-right'), 10);
+    var zindex = 2000;
+    var modal_list = $('.modal'); // get a list of all existing modals
+    var $body = $('body');
+    var originalBodyPad = parseInt($body.css('padding-right'), 10);
 
-		var measureScrollbar = function () {
-			// almost exact copy of source of modal.js
-			var scrollDiv = document.createElement('div');
-			scrollDiv.className = 'modal-scrollbar-measure';
-			$body.append(scrollDiv);
-			var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
-			$body[0].removeChild(scrollDiv);
-			return scrollbarWidth;
-		};
+    var measureScrollbar = function () {
+        // almost exact copy of source of modal.js
+        var scrollDiv = document.createElement('div');
+        scrollDiv.className = 'modal-scrollbar-measure';
+        $body.append(scrollDiv);
+        var scrollbarWidth = scrollDiv.offsetWidth - scrollDiv.clientWidth;
+        $body[0].removeChild(scrollDiv);
+        return scrollbarWidth;
+    };
 
-		var attachEvents = function(to) {
-			$(to).on('show.bs.modal', function (e) {
-				// fix z-index -> put this modal on the very top
-				$(this).css('z-index', zindex++);
+    var attachEvents = function(to) {
+        $(to).on('show.bs.modal', function (e) {
+            // fix z-index -> put this modal on the very top
+            zindex = zindex + 2;
+            $(this).css('z-index', zindex);
 
-				// reset orginal padding-right (as if this modal is the first one), so modal-plugin can do it's "thing"
-				$body.css('padding-right', originalBodyPad);
-			});
-			
-			$(to).on('shown.bs.modal', function (e) {
-				// attach backdrop to modal (fix for older bootstrap release)
-				if($(this).find('.modal-backdrop').length === 0) {
-					var $backdrop = $('body > .modal-backdrop').first();
-							
-					$body.append(
-						$('<div />')
-							.css('z-index', zindex++)
-							.append($backdrop)
-							.append(this)
-					);
+            // reset orginal padding-right (as if this modal is the first one), so modal-plugin can do it's "thing"
+            $body.css('padding-right', originalBodyPad);
+        });
 
-					// re-focus
-					$(this).trigger('focus');
-				}
-			});
+        $(to).on('shown.bs.modal', function (e) {
+            // attach backdrop to modal (fix for older bootstrap release)
+            if($(this).find('.modal-backdrop').length === 0) {
+                var $backdrop = $('body > .modal-backdrop').first();
 
-			$(to).on('hidden.bs.modal', function(e) {
-				var $visible_modals = $('.modal:visible'); // get a list of visible modals, after closing this one
+                $backdrop.css('z-index', zindex-1);
 
-				if( $visible_modals.length > 0 ) {
-					// this closing modal reseted the padding, so we need to re-apply it (still open modals!)
-					$body.css('padding-right', originalBodyPad + measureScrollbar());
+                $(this).parent().append($backdrop);
 
-					// if a second modal is closed, the first modal is unscrollable - to fix that,
-					// re-add the following class to the body
-					// credits goes to "H Dog": http://stackoverflow.com/a/30876481/4945333
-					$('body', document).addClass('modal-open');
-					// re-run on older version, to get executed after all 'hidden.bs.modal'-Events are run
-					setTimeout(function() {
-						$('body', document).addClass('modal-open');
-					}, 10);
+                /*$body.append(
+                    $('<div />')
+                        .append($backdrop)
+                        .append(this)
+                );*/
 
-					// focus the top most modal
-					var topMostModal = null;
-					var topMostzIndex = -1;
+                // re-focus
+                $(this).trigger('focus');
+            }
+        });
 
-					$visible_modals.each(function() {
-						var zIndex = $(this).css('z-index');
+        $(to).on('hidden.bs.modal', function(e) {
+            var $visible_modals = $('.modal:visible'); // get a list of visible modals, after closing this one
 
-						if( zIndex > topMostzIndex ) {
-							topMostModal = $(this);
-							topMostzIndex = zIndex;
-						}
-					});
+            if( $visible_modals.length > 0 ) {
+                // this closing modal reseted the padding, so we need to re-apply it (still open modals!)
+                $body.css('padding-right', originalBodyPad + measureScrollbar());
 
-					if(topMostModal) {
-						$(topMostModal).trigger('focus');
-					}
-				}
-				else {
-					// shouldn't be needed, but just in case reset padding on last modal
-					$body.css('padding-right', originalBodyPad);
-				}
-			});
-		};
+                // if a second modal is closed, the first modal is unscrollable - to fix that,
+                // re-add the following class to the body
+                // credits goes to "H Dog": http://stackoverflow.com/a/30876481/4945333
+                $('body', document).addClass('modal-open');
+                // re-run on older version, to get executed after all 'hidden.bs.modal'-Events are run
+                setTimeout(function() {
+                    $('body', document).addClass('modal-open');
+                }, 10);
 
-		attachEvents(modal_list);
+                // focus the top most modal
+                var topMostModal = null;
+                var topMostzIndex = -1;
 
-		// replace $.modal(...) function to get a hold on dynamically created modals
-		var org_modal = $.fn.modal;
-		$.fn.modal = function() {
-			// check each element matched by selector (provided by "this"), if it's
-			// in our modal_list - if not, add it and attach events
-			this.each(function() {
-				var found = false;
-				var $that = $(this);
-				modal_list.each(function(){
-					if( $(this).is($that) ) {
-						found = true;
-					}
-				});
-				if( false === found ) {
-					attachEvents($that);
-					modal_list = modal_list.add($(this));
-				}
-			});
+                $visible_modals.each(function() {
+                    var zIndex = $(this).css('z-index');
 
-			// call orginal $.modal(...) function
-			// 1) inject "this" to the function (~html-elements/jquery-elements)
-			// 2) passthrough any argument
-			return org_modal.apply(this, arguments);
-		};
-	});
+                    if( zIndex > topMostzIndex ) {
+                        topMostModal = $(this);
+                        topMostzIndex = zIndex;
+                    }
+                });
+
+                if(topMostModal) {
+                    $(topMostModal).trigger('focus');
+                }
+            }
+            else {
+                // shouldn't be needed, but just in case reset padding on last modal
+                $body.css('padding-right', originalBodyPad);
+            }
+        });
+    };
+
+    attachEvents(modal_list);
+
+    // replace $.modal(...) function to get a hold on dynamically created modals
+    var org_modal = $.fn.modal;
+    $.fn.modal = function() {
+        // check each element matched by selector (provided by "this"), if it's
+        // in our modal_list - if not, add it and attach events
+        this.each(function() {
+            var found = false;
+            var $that = $(this);
+            modal_list.each(function(){
+                if( $(this).is($that) ) {
+                    found = true;
+                }
+            });
+            if( false === found ) {
+                attachEvents($that);
+                modal_list = modal_list.add($(this));
+            }
+        });
+
+        // call orginal $.modal(...) function
+        // 1) inject "this" to the function (~html-elements/jquery-elements)
+        // 2) passthrough any argument
+        return org_modal.apply(this, arguments);
+    };
 }(jQuery);
